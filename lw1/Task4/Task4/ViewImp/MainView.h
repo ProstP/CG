b8f146c9d4d173112fab847d1be3a2c8b24f1855
DESKTOP-K55KTC9\Prost <prosto_channal@mail.ru>
2025-02-10 15:42:04 +0300

    Lw1-4 Сделал 4 задание с допом

---
+++ lw1/Task4/Task4/ViewImp/MainView.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "../View/IView.h"
+
+using namespace View;
+
+class MainView : public IView
+{
+public:
+	void OnPaint(HWND hwnd) override;
+	void OnLButtonDown(HWND hwnd, int x, int y) override;
+	void OnDestroy(HWND hwnd) override;
+
+	void InitWord(int length) override;
+	void SetQuestion(const std::string& question) override;
+	void InitAlphabet() override;
+	void OpenLetter(int index, char letter) override;
+	void SetLetterStateInAlphabet(char letter, Model::WordStates state) override;
+	void SetMistakeCount(int count) override;
+
+	bool NeedPaint() const override;
+
+	void RegisterObserver(Observer::IObserver<char>& observer) override;
+
+	void NotifyObservers() override;
+
+	void RemoveObserver(Observer::IObserver<char>& observer) override;
+
+private:
+	std::string m_question;
+	char m_lastOpenedLetter;
+	std::vector<std::optional<char>> m_word;
+	int m_mistakeCount = 0;
+	std::map<char, Model::WordStates> m_alphabet;
+	bool m_needPaint = true;
+	std::set<Observer::IObserver<char>*> m_observers;
+
+	void PaintQuestion(HDC hdc, int x, int y);
+	void PaintWord(HDC hdc, int x, int y);
+	void PaintMistakes(HDC hdc, int x, int y);
+	void PaintAlphabet(HDC hdc, int x, int y);
+	void OpenLetter(int x, int y);
+	static std::wstring StringToWstring(const std::string& str);
+	static wchar_t CharToWChar(char ch);
+};
+
